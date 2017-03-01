e7815bb1f5f6b6debd4f105c668f8883c5f860b3
nickedude <niklas.erik.gustafson@gmail.com>
2017-03-01 09:21:15 +0100

    hand in lab 2 to 5

--- Lab 5/Lab5/EDA322Testbench.vhd
+++ Lab 5/Lab5/EDA322Testbench.vhd
@@ -183,7 +183,7 @@ acc_process : PROCESS (acc2seg)
 disp_process : PROCESS (disp2seg)
 	BEGIN
 		IF (aresetn = '1') THEN
-			ASSERT disp2seg /= "11101001"
+			ASSERT disp2seg /= "10010000"
 			REPORT "Test successfull if no previous error!"
 			SEVERITY failure;
 		
